4f20d727016f4c720ee8637eb92320f07c90de4e
Seth Borden <sethborden@gmail.com>
2014-10-24 13:56:14 -0400

    Pushing change to live.

--- publish.bat
+++ publish.bat
@@ -1,9 +1,9 @@
 xcopy ..\sdrsh\dist .\dist /s /e /y
-copy dist\index.html . /s /e /y
-copy dist\*.png . /s /e /y
+copy dist\index.html . /y
+copy dist\*.png . /y 
 sed -i "s/\"css/\"dist\/css/g" index.html
 sed -i "s/all.js/dist\/all.min.js/g" index.html
 git add .
 git commit -m "Pushing change to live."
-git push gh-pages origin
+git push origin gh-pages
 
