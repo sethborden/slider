d38f8545762f575a6941a1596329917a4d0ed4d9
Seth Borden <sethborden@gmail.com>
2014-10-24 13:56:35 -0400

    Pushing change to live.

--- publish.bat
+++ publish.bat
@@ -1,6 +1,7 @@
 xcopy ..\sdrsh\dist .\dist /s /e /y
 copy dist\index.html . /y
 copy dist\*.png . /y 
+copy dist\*.gif . /y 
 sed -i "s/\"css/\"dist\/css/g" index.html
 sed -i "s/all.js/dist\/all.min.js/g" index.html
 git add .
