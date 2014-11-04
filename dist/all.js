98a7d49977f5c0dec6f90252bdfd94a5339ccc6c
Seth Borden <sethborden@gmail.com>
2014-11-04 17:33:07 -0500

    Pushing change to live.

--- dist/all.js
+++ dist/all.js
@@ -1626,13 +1626,31 @@ var el = $(document);
 var vars = {};
 
 el.on('touchstart', function(e) {
-    console.log(e.originalEvent.changedTouches);
+    vars.start = e.originalEvent.changedTouches[0];
+    
 });
 
 el.on('touchmove', function(e) {
-    console.log(e.originalEvent.changedTouches);
 });
 
 el.on('touchend', function(e) {
-    console.log(e.originalEvent.changedTouches);
+    vars.end = e.originalEvent.changedTouches[0];
+    getTouchVector();
 });
+
+function getTouchVector() {
+    var delX, delY, theta, quad;
+    delX = vars.end.screenX - vars.start.screenX;
+    delY = vars.end.screenY - vars.start.screenY;
+    if (delX > 0 && delY > 0) {
+        quad = 0;
+    } else if (delY > 0 && delX < 0) {
+        quad = 1;
+    } else if (delY < 0 && delX < 0) {
+        quad = 2;
+    } else if (delY > 0 && delX < 0) {
+        quad = 3;
+    }
+    theta = (Math.atan2(delY,delX) + quad * Math.PI / 2) * 57.2957795;
+    console.log("\u03b8:", theta + "\u00b0 distance:", Math.sqrt(delX * delX + delY * delY) + "pixels"); 
+}
